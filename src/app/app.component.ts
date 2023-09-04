ca8959f578ab56ace8b31afbf6a30bdddb0d5217
Aleksander Gorecki <core.autocrlf>
2023-09-05 00:21:17 +0200

    projetk am html i troche logiki

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {Task} from "./Task";
+import {Feature} from "./Feature";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'kanban-am';
+  action: string = "";
+  tasks: Task[] = [
+    {
+      id: 1,
+      name: 'backengd projektu',
+      status: 'TODO',
+      featureId: 1
+    },
+    {
+      id: 2,
+      name: 'gui do projektu',
+      status: 'TODO',
+      featureId: 1
+    },
+  ];
+  features: Feature[] = [
+    {
+      id: 1,
+      name: "zdanie am",
+      tasks: [],
+      status: 'IN PROGRESS'
+    },
+  ]
 }
